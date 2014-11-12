5f206706c9f8585a382817c694e85faa7c18246e
spott <atte.saarinen@iki.fi>
2014-11-12 12:04:47 +0200

    .

--- .Trash-1007/files/autoheight.js
+++
@@ -1,6 +0,0 @@
-angular.module('cv2').directive('autoheight', function() {
-    return function (scope, element, attrs) {
-        //element.height($(window).height() - $('.header').outerHeight());
-        console.log('autoheight');
-    }
-});
